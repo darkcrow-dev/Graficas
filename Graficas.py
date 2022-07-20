5e08c41a58503e99bcbe334f6ac9328710df9505
Charly Lopez <shadowlink1993@hotmail.com>
2022-07-19 23:24:25 -0500

    Prueba

--- Graficas.py
+++ Graficas.py
@@ -1,4 +1,5 @@
 """Graficar en python 2 variables"""
+#############################################
 import numpy as np      #Libreria numpy
 import matplotlib.pyplot as plt     #Libreria para graficar
 
